d116ef03762d317acd9430ebfde5b227dc485ef8
dakiman <dvancov@hotmail.com>
2020-08-17 16:20:10 +0200

    changed card styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -58,116 +58,136 @@
         <!--                </div>-->
         <!--            </div>-->
         <!--        </div>-->
-        <div class="container">
+        <div class="container-fluid">
             <div class="row">
-                <div class="col-6">
-                    <b-card
-                        no-body
+                <div class="col-md-6">
+                    <b-overlay
+                        :show="guildsLoading"
+                        opacity="1"
+                        spinner-small
+                        spinner-variant="primary"
                     >
-                        <template v-slot:header>
-                            <h4 class="mb-0">Recently Searched Guilds</h4>
-                        </template>
+                        <b-card
+                            bg-variant="primary"
+                            no-body
+                        >
+                            <template v-slot:header>
+                                <h4 class="mb-0">Recently Searched Guilds</h4>
+                            </template>
 
-                        <!--                        <b-card-body>-->
-                        <!--                            <b-card-title>Card Title</b-card-title>-->
-                        <!--                            <b-card-sub-title class="mb-2">Card Sub Title</b-card-sub-title>-->
-                        <!--                            <b-card-text>-->
-                        <!--                                Some quick example text to build on the card title and make up the bulk of the card's-->
-                        <!--                                content.-->
-                        <!--                            </b-card-text>-->
-                        <!--                        </b-card-body>-->
+                            <!--                        <b-card-body>-->
+                            <!--                            <b-card-title>Card Title</b-card-title>-->
+                            <!--                            <b-card-sub-title class="mb-2">Card Sub Title</b-card-sub-title>-->
+                            <!--                            <b-card-text>-->
+                            <!--                                Some quick example text to build on the card title and make up the bulk of the card's-->
+                            <!--                                content.-->
+                            <!--                            </b-card-text>-->
+                            <!--                        </b-card-body>-->
 
-                        <b-list-group flush>
-                            <b-list-group-item
-                                v-for="guild in this.popularGuilds.recently_searched"
-                                :to="{name: 'singleGuild', params: { region: guild.region, realm: guild.realm, name: guild.name }}"
-                                class="border">
-                                <span class="float-left">
-                                    {{ guild.name | deslug }}
-                                </span>
-                                <span class="float-right">
-                                    {{ guild.realm | deslug }}
-                                </span>
-                            </b-list-group-item>
-                        </b-list-group>
+                            <b-list-group flush>
+                                <b-list-group-item
+                                    v-for="guild in this.popularGuilds.recently_searched"
+                                    :to="{name: 'singleGuild', params: { region: guild.region, realm: guild.realm, name: guild.name }}"
+                                    class="border bg-primary">
+                                        <span class="float-left">
+                                            {{ guild.name | deslug }}
+                                        </span>
+                                    <span class="float-right">
+                                            {{ guild.realm | deslug }}
+                                        </span>
+                                </b-list-group-item>
+                            </b-list-group>
 
-<!--                        <b-card-body>-->
-<!--                            <a href="#" class="card-link">Card link</a>-->
-<!--                            <a href="#" class="card-link">Another link</a>-->
-<!--                        </b-card-body>-->
+                            <!--                        <b-card-body>-->
+                            <!--                            <a href="#" class="card-link">Card link</a>-->
+                            <!--                            <a href="#" class="card-link">Another link</a>-->
+                            <!--                        </b-card-body>-->
 
-<!--                        <b-card-footer>This is a footer</b-card-footer>-->
-                    </b-card>
-                    <b-card
-                        class="mt-3"
-                        no-body
-                    >
-                        <template v-slot:header>
-                            <h4 class="mb-0">Most Viewed Characters</h4>
-                        </template>
+                            <!--                        <b-card-footer>This is a footer</b-card-footer>-->
+                        </b-card>
+                        <b-card
+                            class="mt-3"
+                            bg-variant="primary"
+                            no-body
+                        >
+                            <template v-slot:header>
+                                <h4 class="mb-0">Most Viewed Characters</h4>
+                            </template>
 
-                        <b-list-group flush>
-                            <b-list-group-item
-                                v-for="guild in this.popularGuilds.most_popular"
-                                :to="{name: 'singleGuild', params: { region: guild.region, realm: guild.realm, name: guild.name }}"
-                                class="border">
-                                <span class="float-left">
-                                    {{ guild.name | deslug }}
-                                </span>
-                                <span class="float-right">
-                                    {{ guild.realm | deslug }}
-                                </span>
-                            </b-list-group-item>
-                        </b-list-group>
+                            <b-list-group flush>
+                                <b-list-group-item
+                                    v-for="guild in this.popularGuilds.most_popular"
+                                    :to="{name: 'singleGuild', params: { region: guild.region, realm: guild.realm, name: guild.name }}"
+                                    class="border bg-primary">
+                                        <span class="float-left">
+                                            {{ guild.name | deslug }}
+                                        </span>
+                                    <span class="float-right">
+                                            {{ guild.realm | deslug }}
+                                        </span>
+                                </b-list-group-item>
+                            </b-list-group>
 
-                    </b-card>
+                        </b-card>
+
+                    </b-overlay>
                 </div>
-                <div class="col-6">
-                    <b-card
-                        no-body
+                <div class="col-md-6">
+                    <b-overlay
+                        :show="charactersLoading"
+                        opacity="1"
+                        spinner-small
+                        spinner-variant="primary"
+                        bg-color="primary"
                     >
-                        <template v-slot:header>
-                            <h4 class="mb-0">Recently Searched Characters</h4>
-                        </template>
+                        <b-card
+                            bg-variant="primary"
+                            no-body
+                        >
+                            <template v-slot:header>
+                                <h4 class="mb-0">Recently Searched Characters</h4>
+                            </template>
 
-                        <b-list-group flush>
-                            <b-list-group-item
-                                v-for="character in this.popularCharacters.recently_searched"
-                                :to="{name: 'singleCharacter', params: { region: character.region, realm: character.realm, name: character.name }}"
-                                class="border">
+                            <b-list-group flush>
+                                <b-list-group-item
+                                    v-for="character in this.popularCharacters.recently_searched"
+                                    :to="{name: 'singleCharacter', params: { region: character.region, realm: character.realm, name: character.name }}"
+                                    class="border bg-primary">
                                 <span class="float-left">
                                     {{ character.name | deslug }}
                                 </span>
-                                <span class="float-right">
+                                    <span class="float-right">
                                     {{ character.realm | deslug }}
                                 </span>
-                            </b-list-group-item>
-                        </b-list-group>
+                                </b-list-group-item>
+                            </b-list-group>
 
-                    </b-card>
-                    <b-card
-                        class="mt-3"
-                        no-body
-                    >
-                        <template v-slot:header>
-                            <h4 class="mb-0">Most Viewed</h4>
-                        </template>
+                        </b-card>
+                        <b-card
+                            class="mt-3"
+                            bg-variant="primary"
+                            no-body
+                        >
+                            <template v-slot:header>
+                                <h4 class="mb-0">Most Viewed</h4>
+                            </template>
 
-                        <b-list-group flush>
-                            <b-list-group-item
-                                v-for="character in this.popularCharacters.most_popular"
-                                :to="{name: 'singleCharacter', params: { region: character.region, realm: character.realm, name: character.name }}"
-                                class="border">
+                            <b-list-group flush>
+                                <b-list-group-item
+                                    v-for="character in this.popularCharacters.most_popular"
+                                    :to="{name: 'singleCharacter', params: { region: character.region, realm: character.realm, name: character.name }}"
+                                    class="border bg-primary">
                                 <span class="float-left">
                                     {{ character.name | deslug }}
                                 </span>
-                                <span class="float-right">
+                                    <span class="float-right">
                                     {{ character.realm | deslug }}
                                 </span>
-                            </b-list-group-item>
-                        </b-list-group>
+                                </b-list-group-item>
+                            </b-list-group>
 
-                    </b-card>
+                        </b-card>
+                    </b-overlay>
                 </div>
             </div>
         </div>
@@ -182,6 +202,8 @@
         name: 'home',
         data() {
             return {
+                guildsLoading: true,
+                charactersLoading: true,
                 popularGuilds: {},
                 popularCharacters: {}
             }
@@ -189,12 +211,12 @@
         mounted() {
             GuildService.getPopularGuilds()
                 .then(({data}) => {
-                    console.log(data);
                     this.popularGuilds = data;
+                    this.guildsLoading = false;
                 })
             CharacterService.getPopularCharacters()
                 .then(({data}) => {
-                    console.log(data);
+                    this.charactersLoading = false
                     this.popularCharacters = data;
                 })
         }
